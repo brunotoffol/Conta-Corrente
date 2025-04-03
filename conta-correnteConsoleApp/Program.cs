92d292d604f983af55568feb2157a4ac2de687c3
Bruno De Toffol <msn-bruno@hotmail.com>
2025-04-03 09:45:08 -0300

    Add project files.

---
+++ conta-correnteConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace conta_correnteConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Características de uma Conta corrente: Um número de identificação único, saldo disponível, limite de débito e registo de movimentações financeiras (histórico);
+             * A conta corrente deve poder realizar as seguintes operações: 
+                - Saque(Retirada de valores, respeita um limite)
+                - Depósito(Entrada de valores)
+                - Consulta do Saldo
+                - Emissão de Extrato: Basicamente o histórico de operações
+                - Transfêrência entre contas: envio de valores para outras contas
+            */
+
+
+        }
+    }
+}
