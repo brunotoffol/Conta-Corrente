11923c6d0c25101f00dafade3128e1230e6b1627
Bruno De Toffol <msn-bruno@hotmail.com>
2025-04-03 11:03:15 -0300

    Adicionado o código de exemplo de uso forneceido pelo Thiago.

--- conta-correnteConsoleApp/Program.cs
+++ conta-correnteConsoleApp/Program.cs
@@ -13,6 +13,33 @@
                 - Transfêrência entre contas: envio de valores para outras contas
             */
 
+            ContaCorrente conta1 = new ContaCorrente();
+            conta1.saldo = 1000;
+            conta1.numero = 12;
+            conta1.limite = 0;
+            conta1.movimentacoes = new ModuleHandle[10];
+
+            conta1.Sacar(200);
+
+            conta1.Depositar(300);
+
+            conta1.Depositar(500);
+
+            conta1.Sacar(200);
+
+            ContaCorrente conta2 = new ContaCorrente();
+            conta2.saldo = 300;
+            conta2.numero = 13;
+            conta2.limite = 0;
+            conta2.movimentacoes = new Movimentacao[10];
+
+            conta1.TransferirPara(conta2, 400);
+
+            conta1.ExibirExtrato();
+
+            Console.WriteLine();
+
+            conta2.ExibirExtrato();
 
         }
     }
